45a80d1cf977bba01fb97b9cbbf05f40b54fe964
devanshtivari <devansh80771@gmail.com>
2022-09-08 18:21:10 +0530

    made changes in the filesystem

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-document.addEventListener("mouseup", (e) => {
-})
+const textarea = document.getElementById("text-area");
+
+function send_tweet(){
+    if(textarea.value.length === 0){
+        launch_toast("Please enter a text");
+        return;
+    }   
+    if(document.getSelection().toString()){
+        const selection = document.getSelection().toString();
+        const URL = location.href;
+        const finalurl = `https://twitter.com/intent/tweet?hashtags=text&url=${URL}&text=${selection}`;
+        window.open(finalurl, '_blank');
+    }
+}
+
+function launch_toast(msg) {
+  var x = document.getElementById("toast");
+  var text = document.getElementById("desc");
+  text.innerText = msg;
+  x.className = "show";
+  setTimeout(function () {
+    x.className = x.className.replace("show", "");
+  }, 5000);
+}
